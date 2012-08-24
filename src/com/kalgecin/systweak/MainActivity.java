326da2a35d05370cbc600b3d07c8aff543202df8
Michael Kitange <kalgecin@gmail.com>
2012-08-24 14:17:16 +0300

    code cleanup

--- src/com/kalgecin/systweak/MainActivity.java
+++ src/com/kalgecin/systweak/MainActivity.java
@@ -12,12 +12,36 @@ import android.widget.Button;
 import android.widget.CheckBox;
 
 public class MainActivity extends Activity {
-
+	private settingsDB dataSrc;
+	CheckBox chkRomManager;
+	CheckBox chkLiveWallpapers;
+	CheckBox chkCMWallpapers;
+	CheckBox chkGTTS;
+	CheckBox chkMovie;
+	CheckBox chkGmail;
+	CheckBox chkTvOut;
+	String[] checks = {"chkRomManager","chkLiveWallpapers","chkCMWallpapers","chkGTTS","chkMovie","chkGmail","chkTvOut"};
+	String[] CHKnames = {"com.koushikdutta.rommanager","com.android.wallpaper.livepicker","com.cyanogenmod.CMWallpapers",
+						"com.google.android.tts","com.movie","com.google.android.gm","com.teamhacksung.tvout"};
+	CheckBox[] CBchecks = {chkRomManager,chkLiveWallpapers,chkCMWallpapers,chkGTTS,chkMovie,chkGmail,chkTvOut};
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button btnSet 				= (Button) findViewById(R.id.btnSetOnBoot);
+        Button btnSet 		= (Button) findViewById(R.id.btnSetOnBoot);
+        chkRomManager 		= (CheckBox) findViewById(R.id.chkRomManager);
+        chkLiveWallpapers 	= (CheckBox) findViewById(R.id.chkLiveWallpapers);
+        chkCMWallpapers 	= (CheckBox) findViewById(R.id.chkCMWallpapers);
+        chkGTTS 			= (CheckBox) findViewById(R.id.chkGTTS);
+        chkMovie 			= (CheckBox) findViewById(R.id.chkMovieStudio);
+        chkGmail 			= (CheckBox) findViewById(R.id.chkGmail);
+        chkTvOut			= (CheckBox) findViewById(R.id.chkTvOut);
+        
+        dataSrc = new settingsDB(this);
+        dataSrc.open();
+        loadChecks();
+        
         btnSet.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
@@ -25,94 +49,28 @@ public class MainActivity extends Activity {
 			}
 		});
     }
+    public void loadChecks(){
+    	for(int i=0;i<checks.length;i++){
+    		CBchecks[i].setChecked(Boolean.getBoolean(dataSrc.getSetting(checks[i])));
+    	}
+    	
+    }
     public void SetChecks(){
-    	 CheckBox chkRomManager 	= (CheckBox) findViewById(R.id.chkRomManager);
-         CheckBox chkLiveWallpapers = (CheckBox) findViewById(R.id.chkLiveWallpapers);
-         CheckBox chkCMWallpapers 	= (CheckBox) findViewById(R.id.chkCMWallpapers);
-         CheckBox chkGTTS 			= (CheckBox) findViewById(R.id.chkGTTS);
-         CheckBox chkMovie 			= (CheckBox) findViewById(R.id.chkMovieStudio);
-         CheckBox chkGmail 			= (CheckBox) findViewById(R.id.chkGmail);
-         CheckBox chkTvOut			= (CheckBox) findViewById(R.id.chkTvOut);
+    	 
          ProcessBuilder cmd;
          Process process;
          try{
         	 String[] args = {"su","-c","pm","enable",""};
-	         if(chkRomManager.isChecked()){
-	        	 //enable RomManager
-	        	 args[3] = "enable";
-	        	 args[4] = "com.koushikdutta.rommanager";
-	         }else{
-	        	 //disable RomManager
-	        	 args[3] = "disable";
-	        	 args[4] = "com.koushikdutta.rommanager";
-	         }
-	         cmd = new ProcessBuilder(args);
-        	 process = cmd.start();
-        	 
-	         if(chkLiveWallpapers.isChecked()){
-	        	 //enable LiveWallpapers
-	        	 args[3] = "enable";
-	        	 args[4] = "com.android.wallpaper.livepicker";
-	         }else{
-	        	 //disable LiveWallpapers
-	        	 args[3] = "disable";
-	        	 args[4] = "com.android.wallpaper.livepicker";
-	         }
-	         cmd = new ProcessBuilder(args);
-        	 process = cmd.start();
-        	 
-	         if(chkCMWallpapers.isChecked()){
-	        	 //enable CM Wallpapers 
-	        	 args[3] = "enable";
-	        	 args[4] = "com.cyanogenmod.CMWallpapers";
-	         }else{
-	        	 //disable CM Wallpapers
-	        	 args[3] = "disable";
-	        	 args[4] = "com.cyanogenmod.CMWallpapers";
-	         }
-	         cmd = new ProcessBuilder(args);
-        	 process = cmd.start();
-        	 
-        	 if(chkTvOut.isChecked()){
-        		 args[3] = "enabled";
-        		 args[4] = "com.teamhacksung.tvout";
-        	 }else{
-        		 args[3] = "disabled";
-        		 args[4] = "com.teamhacksung.tvout";
+        	 for(int i=0;i<checks.length;i++){
+        		 args[4] = CHKnames[i];
+        		 if(CBchecks[i].isChecked()){
+        			 args[3] = "enabled";
+        		 }else{
+        			 args[3] = "disabled";
+        		 }
+        		 cmd = new ProcessBuilder(args);
+            	 process = cmd.start();
         	 }
-        	 cmd = new ProcessBuilder(args);
-        	 process = cmd.start();
-        	 
-	         if(chkGTTS.isChecked()){
-	        	 //enable Google Text-To-Speech
-	        	 args[3] = "enabled";
-	        	 args[4] = "com.google.android.tts";
-	         }else{
-	        	 //disable Google Text-To-Speech
-	        	 args[3] = "disabled";
-	        	 args[4] = "com.google.android.tts";
-	         }
-	         cmd = new ProcessBuilder(args);
-	         process = cmd.start();
-	         
-	         //TODO: implement movie editor
-	         if(chkMovie.isChecked()){
-	        	 //enable Movie Editor
-	         }else{
-	        	 //disable Movie Editor
-	         }
-	         if(chkGmail.isChecked()){
-	        	 //enable Gmail
-	        	 args[3] = "enable";
-	        	 args[4] = "com.google.android.gm";
-	         }else{
-	        	 //disable GMail
-	        	 args[3] = "disable";
-	        	 args[4] = "com.google.android.gm";
-	         }
-	         cmd = new ProcessBuilder(args);
-	         process = cmd.start();
-	         
          }catch(IOException e){}
     }
     @Override
