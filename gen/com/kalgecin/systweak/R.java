be0a0f8a5c0df977d3b4f5508fd7e000e6081650
Michael Kitange <kalgecin@gmail.com>
2012-09-01 00:40:07 +0300

    added German. Thanks to KcDaRookie

--- gen/com/kalgecin/systweak/R.java
+++ gen/com/kalgecin/systweak/R.java
@@ -66,6 +66,7 @@ public final class R {
  Things to toggle 
  Things to toggle 
  Things to toggle 
+ Things to toggle 
          */
         public static final int RomManager=0x7f040009;
         public static final int TVOut=0x7f04000e;
@@ -74,14 +75,20 @@ public final class R {
         public static final int about_translations=0x7f04001c;
         public static final int about_translations_thanks=0x7f04001e;
         /**  Strings for 'all' section 
+ Strings for 'all' section 
+ Strings for 'all' section 
          */
         public static final int all_apps=0x7f040002;
         public static final int apollo=0x7f040010;
         /**  end 
+ end 
+ end 
          */
         public static final int app_name=0x7f040004;
         public static final int apply=0x7f04001a;
         /**  Not yet added 
+ end 
+ Not yet added 
  end 
  Not yet added 
  end 
