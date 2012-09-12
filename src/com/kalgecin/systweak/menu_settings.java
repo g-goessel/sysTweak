e6fe5f3f37ec12b3ee582978dbc2fa284652c817
Michael Kitange <kalgecin@gmail.com>
2012-09-12 09:18:21 +0400

    added non functional 'show advanced' option to show dangerous stuff

--- src/com/kalgecin/systweak/menu_settings.java
+++ src/com/kalgecin/systweak/menu_settings.java
@@ -9,6 +9,7 @@ import android.widget.CheckBox;
 public class menu_settings extends MainActivity{
 	private settingsDB dataSrc;
 	private CheckBox chkOnBoot;
+	private CheckBox chkShowAdv;
 	@Override
 	public void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
@@ -18,22 +19,24 @@ public class menu_settings extends MainActivity{
 		dataSrc.open();
 		
 		chkOnBoot = (CheckBox) findViewById(R.id.chkSettings_onboot);
+		chkShowAdv = (CheckBox) findViewById(R.id.chkShowAdv);
 		Button btnSet = (Button) findViewById(R.id.btnSettingsSave);
 		loadSettings();
 		btnSet.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				saveSettings();
 			}
 		});
 	}
 	private void saveSettings(){
 		dataSrc.addSetting("on_boot", Boolean.toString(chkOnBoot.isChecked()));
+		dataSrc.addSetting("showadv", Boolean.toString(chkShowAdv.isChecked()));
 	}
 	private void loadSettings(){
 		String onB = dataSrc.getSetting("on_boot");
 		boolean onboot = Boolean.parseBoolean(onB);
 		chkOnBoot.setChecked(onboot);
+		chkShowAdv.setChecked(Boolean.parseBoolean(dataSrc.getSetting("showadv")));
 	}
 }
